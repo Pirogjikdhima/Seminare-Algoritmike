d6fcb445b72f6ecd3905e7441b4c9060268f48a0
Pirogjikdhima <111192631+Pirogjikdhima@users.noreply.github.com>
2024-12-18 15:07:18 +0100

    Added Final Lab3

---
+++ src/Laboratore/Lab4/LRUCache.java
@@ -0,0 +1,20 @@
+//package Laboratore.Lab3;
+//
+//public class LRUCache<Item> {
+//
+//    public LRUCache() {
+//    }
+//
+//    public int size() {
+//
+//    }
+//
+//    public void access(Item item) {
+//
+//    }
+//
+//    public Item remove() {
+//
+//    }
+//}
+////
